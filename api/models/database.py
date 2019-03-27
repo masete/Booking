66579a7865b67f648ab2751268f46b6901c9a905
Masete <nicholasmasete72@gmail.com>
2019-03-27 17:24:42 +0300

    Restoring old app

--- api/models/database.py
+++ api/models/database.py
@@ -51,15 +51,7 @@ class DatabaseConnection:
         try:
 
             if os.getenv("FLASK_ENV") == "production":
-                self.credentials_heroku = """
-                            dbname='deipk3r58od4el'
-                            user= 'aknlwxnofjeegn'
-                            password = '538e8ddb510fc957f338a5b5ce6c0a941b8a4c17ee95286fa1301738d83e4632'
-                            host='ec2-50-19-109-120.compute-1.amazonaws.com'
-                            port =5432
-
-                            """
-                self.credentials = self.credentials_heroku
+                self.connection = psycopg2.connect(os.getenv("DATABASE_URL"), cursor_factory=RealDictCursor)
 
             elif os.getenv("FLASK_ENV") == "TESTING":
                 print('Connecting to test db')
@@ -85,9 +77,9 @@ class DatabaseConnection:
         except Exception as error:
             print(f"error: {error}")
 
-        # self.cursor.execute("SELECT * FROM users WHERE email= '{}'".format("admin@gmail.com"))
-        # if self.cursor.fetchone():
-        #     return None
+        self.cursor.execute("SELECT * FROM users WHERE email= '{}'".format("admin@gmail.com"))
+        if self.cursor.fetchone():
+            return None
         # set admin user
 
         sql = "INSERT INTO users(username, email, password, as_admin) VALUES('admin', 'admin@gmail.com', 12345, True)"
