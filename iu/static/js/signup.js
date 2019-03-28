390eb9e3204c64cedf6438202be4036766e08079
Masete <nicholasmasete72@gmail.com>
2019-03-28 13:47:28 +0300

    just trying out

--- iu/static/js/signup.js
+++ iu/static/js/signup.js
@@ -22,28 +22,28 @@ function signUpAccount() {
     })
         .then((response) => response.json())
         .then((data) => {
-            if (data.status === 400) {
-                //Bad format data
-                displayError(data.error);
-
-
-            } else if (data.status === 409) {
-                //Duplicate username, email or phone number
-                displayError(data.error);
-
-            } else if (data.status === 201) {
-                //on success
-
-                document.getElementById("message").style.display = "block";
-                document.getElementById("message").innerHTML = data["data"][0].success;
-//                window.setTimeout(function () {
-//                    window.location.replace("../index.html");
-//                }, 3000);
-
-            }
-
-
-        })
+//            if (data.status === 400) {
+//                //Bad format data
+//                displayError(data.error);
+//
+//
+//            } else if (data.status === 409) {
+//                //Duplicate username, email or phone number
+//                displayError(data.error);
+//
+//            } else if (data.status === 201) {
+//                //on success
+//
+            document.getElementById("message").style.display = "block";
+            document.getElementById("message").innerHTML = data["data"][0].success;
+            window.setTimeout(function () {
+                window.location.replace("../index.html");
+            }, 3000);
+//
+//            }
+//
+//
+//        })
 //        .catch((error) => console.log(error));
 
 }
